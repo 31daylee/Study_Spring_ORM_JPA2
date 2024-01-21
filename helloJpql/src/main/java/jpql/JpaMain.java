b975b7140e00c17d25df96407bacaf722cdc55cc
gussla2206 <gussla2206@gmail.com>
2024-01-22 02:11:50 +0900

    session 10 기본문법과 쿼리 API

--- helloJpql/src/main/java/jpql/JpaMain.java
+++ helloJpql/src/main/java/jpql/JpaMain.java
@@ -1,9 +1,7 @@
 package jpql;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -19,8 +17,17 @@ public class JpaMain {
 
             Member member = new Member();
             member.setUsername("member2");
+            member.setAge(10);
             em.persist(member);
 
+            TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class); // member 클래스 전체
+            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class); // String 이란 확실한 타입이 존재하는 경우
+            Query query3 = em.createQuery("select m.username, m.age from Member m"); // 타입이 다중인 경우 -> 반환타입이 불명확
+
+            List<Member> resultList = query.getResultList(); // 리턴되는 값이 여러 개일때
+            for(Member m : resultList){
+                System.out.println("member = "+ m);
+            }
             tx.commit(); // [트랜잭션] 커밋
         }catch (Exception e){
             tx.rollback();
