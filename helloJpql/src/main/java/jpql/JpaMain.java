d222fbb3eede7c21227cd505044573475a72219a
gussla2206 <gussla2206@gmail.com>
2024-01-23 22:53:49 +0900

    session11 페지조인1 - 기본

--- helloJpql/src/main/java/jpql/JpaMain.java
+++ helloJpql/src/main/java/jpql/JpaMain.java
@@ -1,6 +1,7 @@
 package jpql;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 
@@ -15,19 +16,43 @@ public class JpaMain {
         tx.begin(); // [트랜잭션] 시작
 
         try {
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
 
-            Member member = new Member();
-            member.setUsername("member2");
-            member.setAge(10);
-            member.setTeam(team);
-            member.setType(MemberType.ADMIN);
-            em.persist(member);
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
+
+            String query = "select m from Member m join fetch m.team"; //
+            List<Member> result = em.createQuery(query, Member.class).getResultList();
+
+            for(Member member : result){
+                System.out.println("member = "+ member.getUsername() + " , "+ member.getTeam().getName());
+                // 회원1, 팀A(SQL)
+                // 회원2, 팀A(1차캐시)
+                // 회원3, 팀B(SQL)
+
+                // if 회원 100명 -> N + 1 발생 ->fetch 조인 사용
+            }
+
 
             tx.commit(); // [트랜잭션] 커밋
         }catch (Exception e){
