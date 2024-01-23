ae4f3d3310bff2f747eeab194820c2b306f3386f
gussla2206 <gussla2206@gmail.com>
2024-01-23 22:30:43 +0900

    session11

--- helloJpql/src/main/java/jpql/JpaMain.java
+++ helloJpql/src/main/java/jpql/JpaMain.java
@@ -26,49 +26,8 @@ public class JpaMain {
             member.setType(MemberType.ADMIN);
             em.persist(member);
 
-            /*TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class); // member 클래스 전체
-            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class); // String 이란 확실한 타입이 존재하는 경우
-            Query query3 = em.createQuery("select m.username, m.age from Member m"); // 타입이 다중인 경우 -> 반환타입이 불명확
-
-            List<Member> resultList = query.getResultList(); // 리턴되는 값이 여러 개일때
-            for(Member m : resultList){
-                System.out.println("member = "+ m);
-            }*/
-
             em.flush();
             em.clear();
-            // session 10 프로젝션
-           /* List<MemberDTO> result = em.createQuery("select new jpql.MemberDTO(m.username,m.age) from Member m", MemberDTO.class)
-                    .getResultList();
-
-            MemberDTO memberDTO = result.get(0);
-            System.out.println("memberDTO = "+memberDTO.getUsername());
-            System.out.println("memberDTO = "+memberDTO.getAge());*/
-
-            // session 10 페이징
-            /*List<Member> result = em.createQuery("select m from Member m order by m.age desc ", Member.class)
-                        .setFirstResult(0)
-                        .setMaxResults(10)
-                        .getResultList();
-
-            for(Member member1 : result){
-                System.out.println("member1 = "+member1);
-            }*/
-            // session10 조인
-            /*String query = "select m from Member m inner join m.team t where t.name = :teamName";
-
-            List<Member> result = em.createQuery(query, Member.class)
-                    .getResultList();*/
-            String query = "select m.username, 'HELLO', true from Member m "+
-                            "where m.type = jpql.MemberType.ADMIN";
-
-            List<Object[]> result = em.createQuery(query).getResultList();
-
-            for(Object[] objects : result){
-                System.out.println("objects = "+ objects[0]);
-                System.out.println("objects = "+ objects[1]);
-                System.out.println("objects = "+ objects[2]);
-            }
 
             tx.commit(); // [트랜잭션] 커밋
         }catch (Exception e){
